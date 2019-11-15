8ec5331931e34514ab00bdad71c4c3fcf70fcdf5
naufalrandhika88 <45902571+naufalrandhika88@users.noreply.github.com>
2019-11-15 09:52:38 +0700

    commit pertama nopal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'news', loadChildren: './news/news.module#NewsPageModule' },
-  { path: 'news-single', loadChildren: './news-single/news-single.module#NewsSinglePageModule' }
+  { path: "", loadChildren: "./tabs/tabs.module#TabsPageModule" },
+  { path: "news", loadChildren: "./news/news.module#NewsPageModule" },
+  {
+    path: "news-single",
+    loadChildren: "./news-single/news-single.module#NewsSinglePageModule"
+  }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,11 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
+
 export const environment = {
   production: false,
-  apiUrl: 'https://newsapi.org/v2',
-  apiKey: 'b7dd4f5b281643c49849d80fe125234a'
+  apiUrl: "https://newsapi.org/v2",
+  apiKey: "b6221a427d4243929a5449b21632dd51"
 };
 
 /*
